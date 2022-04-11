97f836cc139f00f51c15b1aa7f0879625b9ffdb3
Alison Harman <alison_harman@sympatico.ca>
2022-04-11 18:03:01 +0000

    create and count number of stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -2,5 +2,10 @@ require_relative '../setup'
 
 puts "Exercise 1"
 puts "----------"
+store1 = Store.create(annual_revenue: 300000, mens_apparel: true, womens_apparel: true )
+store2 = Store.create(annual_revenue: 1260000, mens_apparel: false, womens_apparel: true )
+store3 = Store.create(annual_revenue: 190000, mens_apparel: true, womens_apparel: false )
+
+puts Store.count
 
 # Your code goes below here ...
